534aafd7e585d39c355014ce186d9e7a6b63b248
sangeetha2521 <sangeens8101@gmail.com>
2023-05-25 22:58:27 +0530

    About content added

--- src/components/Portfolio/homepage/index.js
+++ src/components/Portfolio/homepage/index.js
@@ -2,13 +2,42 @@ import React from "react";
 import Header from "./header/header";
 import Profile from "./Profile";
 import Projects from "./projects";
+import About from "./about";
+import Contact from "./contact";
+import Skils from "./skils/Skils";
+import { setDarkTheme } from "../../../reducers/slice";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function HomePage() {
+  const theme = useSelector((state) => state.portFolio.defaultTheme);
+  const dispatch = useDispatch();
+  const [className, setClassName] = useState("");
+
+  const onClickChangeTheme = () => {
+    if (theme === "light") {
+      dispatch(setDarkTheme("black"));
+    } else {
+      dispatch(setDarkTheme("light"));
+    }
+  };
+
+  useEffect(() => {
+    setClassName(
+      theme === "light"
+        ? " w-full h-full text-black text-sm h-full bg-gradient-to-r from-[#e7f3ff] to-[#ffffff]"
+        : "w-full h-full bg-black text-white"
+    );
+  }, [theme]);
   return (
-    <div className="h-full bg-gradient-to-r from-[#e7f3ff] to-[#ffffff]">
+    <div className={className}>
       <Header />
+
       <Profile />
+      <About />
       <Projects />
+      <Contact />
+      <Skils />
     </div>
   );
 }
