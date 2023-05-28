6a3fc24dccedbb09dfe9f2642306442d1a22fa68
sangeetha2521 <sangeens8101@gmail.com>
2023-05-28 22:02:28 +0530

    space added

--- src/components/Portfolio/SingleHomePage/index.js
+++ src/components/Portfolio/SingleHomePage/index.js
@@ -32,6 +32,7 @@ export default function SingleHomePage() {
     );
   }, [theme]);
 
+  
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-col flex-grow">
