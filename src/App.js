2e2755aaf21a980f018f4a138b7de72b155dba3e
Sang <sangeetha@konnectify.co>
2023-06-29 18:24:27 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -6,36 +6,32 @@ import About from "./components/Portfolio/homepage/about";
 import HomePage from "./components/Portfolio/homepage";
 import Services from "./components/Portfolio/homepage/services/indes";
 import Skills from "./components/Portfolio/homepage/skils/Skils";
-import Portfolio from "./components/Portfolio";
-import Profile from "./components/Portfolio/homepage/Profile";
+
 import SingleHomePage from "./components/Portfolio/SingleHomePage";
 import SingleHomePageProfile from "./components/Portfolio/SingleHomePage/Profile";
 import SingleHomePageSkills from "./components/Portfolio/SingleHomePage/skils/Skils";
 import SingleHomePageServices from "./components/Portfolio/SingleHomePage/services/indes";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SingleHomePage />}>
-          <Route path="/Home" index element={<><SingleHomePageProfile /><SingleHomePageSkills /><SingleHomePageServices/></>} />
+          <Route path="Home" index element={<><SingleHomePageProfile /><SingleHomePageSkills /><SingleHomePageServices/></>} />
           <Route path="Skills" element={<Skills />} />
           <Route path="Services" element={<Services />} />
           <Route path="Contact" element={<Contact />} />
           <Route path="Projects" element={<Projects />} />
           <Route path="About" element={<About />} />
         </Route>
-        <Route path="*" element={<NoMatch />} /> {/* Optional: Render a "NoMatch" component for invalid routes */}
+        <Route path="*" element={<Navigate to="Home" replace={true} />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-function NoMatch() {
-  return <h1>404 Not Found</h1>;
-  {
-    /* Optional: Define a "NoMatch" component */
-  }
-}
+
+
 
 export default App;
