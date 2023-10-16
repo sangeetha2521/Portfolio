c4cbc72130e1d505bf7f773b7a478eaccdfc5c66
Sang <sangeetha@konnectify.co>
2023-10-16 15:44:11 +0530

    Project added

--- src/components/Portfolio/SingleHomePage/projects/index.js
+++ src/components/Portfolio/SingleHomePage/projects/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Projects() {
-  return <div className="mt-5  flex justify-center items-center h-screen">Comming soon!</div>;
+  return (
+    <div className="mt-5  flex justify-center items-center h-screen">
+      <div className="grid grid-cols-4 p-2 ml-7"></div>
+    </div>
+  );
 }
